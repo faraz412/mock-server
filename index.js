94eb35bf249c314752e224df4976db59df6c9bc1
faraz412 <ahmedfaraz412@gmail.com>
2023-04-21 13:32:16 +0530

    deploy fix

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const server = jsonServer.create();
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const cors = require('cors');
+
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
 
 server.use(cors())
 server.use(jsonServer.bodyParser)
@@ -13,6 +13,4 @@ server.use(router)
 
 const PORT = 8000
 
-server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
-})
+server.listen(PORT);
